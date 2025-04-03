68bd2b249c8d1774cf5df27ee745b87d7cd173ae
sebasoutherncode <sebastian.davila@southerncode.us>
2025-04-03 16:55:31 -0300

    first commit

---
+++ app/langfuse_config.py
@@ -0,0 +1,10 @@
+# langfuse_config.py
+
+import os
+from langfuse import Langfuse
+
+langfuse = Langfuse(
+    public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
+    secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
+    host=os.getenv("LANGFUSE_HOST", "https://cloud.langfuse.com")
+)
